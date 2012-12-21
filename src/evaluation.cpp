25027f5c301c6bcb31e9850906768fc86eda2969
Jeff Chapman II <jac@JAChapmanII.net>
2012-12-21 18:03:24 -0500

    Comment the TokenFragment splitting code

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -255,24 +255,26 @@ Token parse(string statement) {
 	char stringType = '\0';
 	TokenFragment ctoken;
 	while(statement.length() > 0) {
-		//cout << (inToken ? "inToken" : "notToken") << " " <<
-			//(inString ? "inString" : "notString") <<
-			//": " << statement << endl;
-
+		// if the character is a string edge
 		if(statement.front() == '\'' || statement.front() == '"') {
 			if(inString) {
-				// end current string
+				// if we're in a string and it's the end
 				if(statement.front() == stringType) {
+					// if there's string content, add it
 					if(!ctoken.text.empty())
 						stokens.push_back(ctoken);
 					ctoken.clear();
+					// add the special string terminator
 					stokens.push_back({ string(1, statement.front()), true });
 					statement = statement.substr(1);
 					inString = false;
 					continue;
 				}
+			// we're starting a string
 			} else {
+				// set the string type accordingly
 				stringType = statement.front();
+				// add the special string starter
 				stokens.push_back({ string(1, statement.front()), true });
 				statement = statement.substr(1);
 				inString = true;
@@ -281,62 +283,83 @@ Token parse(string statement) {
 		}
 
 		if(inString) {
+			// check for special in-string characters
 			bool isSpecial = false;
 			for(auto specialt : specialInString) {
+				// if it is a special token
 				if(startsWith(statement, specialt)) {
+					// if there is a current token, push it first
 					if(!ctoken.text.empty())
 						stokens.push_back(ctoken);
 					ctoken.clear();
+					// push our new special token
 					stokens.push_back({ specialt, true });
 					statement = statement.substr(specialt.length());
 					isSpecial = true;
 					break;
 				}
 			}
+			// do all previous tests again on special
 			if(isSpecial)
 				continue;
 
+			// if we have an escaped character
 			if(statement.front() == '\\') {
+				// skip the delimiter so we get just the real char
 				statement = statement.substr(1);
+				// if we escaped the end of the string it's an oopsie
 				if(statement.empty()) {
 					throw (string)"escaped character at end of string";
 				}
 			}
 
+			// append current character to current token and advance it string
 			ctoken.text += statement.front();
 			statement = statement.substr(1);
 			continue;
 		}
 
+		// check for special tokens
 		bool isSpecial = false;
 		for(auto specialt : special) {
+			// if we're on a special token
 			if(startsWith(statement, specialt)) {
+				// if there's a current token, end it
 				if(!ctoken.text.empty())
 					stokens.push_back(ctoken);
 				ctoken.clear();
+				// add the new special token
 				stokens.push_back({ specialt, true });
 				statement = statement.substr(specialt.length());
 				isSpecial = true;
 				break;
 			}
 		}
+		// if we found a special token, we do all previous tests again
 		if(isSpecial)
 			continue;
 
+		// if it's a space, end of current token and just skip the space
 		if(isspace(statement[0])) {
 			if(!ctoken.text.empty())
 				stokens.push_back(ctoken);
 			ctoken.clear();
+		// otherwise append to current token
 		} else {
+			// if we have an escaped character
 			if(statement.front() == '\\') {
+				// skip the escape character so the real character is added
 				statement = statement.substr(1);
+				// if there's nothing to escape, error
 				if(statement.empty()) {
 					throw (string)"escaped character at end of string";
 				}
 			}
 
+			// append the current character to the text
 			ctoken.text += statement[0];
 		}
+		// advannce in the statement string
 		statement = statement.substr(1);
 	}
 
