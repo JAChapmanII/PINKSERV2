4690935e49bbcfe7e3887e0a90392293b7decfa7
Jeff Chapman II <jac@JAChapmanII.net>
2013-02-03 10:09:30 -0500

    Use jac as default nick for rpscript

--- bin/rpscript.sh
+++ bin/rpscript.sh
@@ -3,13 +3,15 @@
 script="$1"
 [[ -z $(echo $script | grep -E '.pbrane$') ]] && script="${script}.pbrane"
 
+nick="${2-jac}"
+
 if [[ ! -f ./bin/teval ]]; then
 	echo "./bin/teval does not exist, make sure you make'd!"
 	exit 1
 fi
 
 cat "pscript/$script" | grep -vE '^\s*$' | while read -r line; do
-	echo "$2"
+	echo "$nick"
 	echo "$line"
 done | ./bin/teval
 
