2f55c2b62407bab7731841ba719643eabcb371f1
Jeff Chapman II <jac@jac2.net>
2013-07-20 07:03:44 -0400

    Make rpbrane slightly more intelligent

--- bin/rpbrane.sh
+++ bin/rpbrane.sh
@@ -3,6 +3,11 @@
 nick=${1-jac}
 chan=${2-3dpe}
 
+if [[ ! -f ./bin/pbrane ]]; then
+	echo "./bin/pbrane does not exist, make sure you make'd!"
+	exit 1
+fi
+
 cat /dev/stdin | sed -ur "s/(.+)/:$nick!$nick@host PRIVMSG #$chan :\\1/" | \
 	./bin/pbrane
 
