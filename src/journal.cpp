8c0d66e7960bf3090613deb5c0aff581d26c22a8
Jeff Chapman II <jac@jac2.net>
2013-09-19 07:21:50 -0400

    Major journal cleanup; see full log
    Effort has been made to cache the results of parsing log lines. The parsing is
    still incomplete (only looks for PRIVMSG) but should be significant faster for
    use in rgrep.

--- src/journal.cpp
+++ src/journal.cpp
@@ -16,6 +16,8 @@ using std::cerr;
 #include "util.hpp"
 using util::fromString;
 using util::asString;
+using util::split;
+using util::join;
 #include "config.hpp"
 #include "global.hpp"
 
@@ -30,12 +32,13 @@ bool journal::init() {
 	ifstream jin(config::journalFileName);
 	if(!jin.good())
 		return false;
+
+	string line;
 	while(!jin.eof() && jin.good()) {
-		string line;
 		getline(jin, line);
 		if(line.empty())
 			continue;
-		journal_entries.push_back(Entry::parse(line));
+		journal_entries.push_back(Entry::fromLog(line));
 	}
 	
 	journal_file.open(config::journalFileName, std::ios_base::app);
@@ -52,37 +55,51 @@ void journal::push(Entry nentry) {
 	journal_entries.push_back(nentry);
 	journal_file << nentry.format() << endl;
 }
-std::vector<Entry> journal::search(string regex) {
-	auto start = journal_entries.begin();
-	if(journal_entries.size() > 10000)
-		start = journal_entries.end() - 10000;
 
+std::vector<Entry> journal::search(string regex) {
 	vector<Entry> matches;
 	boost::regex r(regex, boost::regex::perl);
 
-	for(auto i = start; i != journal_entries.end(); ++i) {
-		if(i->type() != EntryType::Text)
+	for(auto i : journal_entries) {
+		if(i.type != EntryType::Text)
 			continue;
-		if(boost::regex_search(i->message(), r, boost::match_default))
-			matches.push_back(*i);
+		if(boost::regex_search(i.arguments, r, boost::match_default))
+			matches.push_back(i);
 	}
 	return matches;
 }
 
-EntryType Entry::type() const {
-	return EntryType::Invalid;
-}
-string Entry::message() const {
-	return this->contents;
+unsigned journal::size() {
+	return journal_entries.size();
 }
 
-Entry::Entry(std::string icontents) : timestamp(global::now()), contents(icontents) {}
-Entry Entry::parse(string line) {
-	Entry result;
-	result.timestamp = fromString<unsigned long>(line.substr(0, line.find("|")));
-	result.contents = line.substr(line.find("|"));
-	return result;
+
+Entry::Entry(string icontents) :
+		contents(icontents) { this->parse(); }
+Entry::Entry(long long itimestamp, string icontents) :
+		timestamp(itimestamp), contents(icontents) { this->parse(); }
+
+Entry Entry::fromLog(string line) {
+	auto timestamp = fromString<long long>(line.substr(0, line.find("|")));
+	auto contents = line.substr(line.find("|") + 1);
+	return { timestamp, contents };
 }
+
+void Entry::parse() {
+	auto parts = split(this->contents, " ");
+	this->who = parts[0];
+	this->where = parts[2];
+
+	this->command = parts[1];
+	if(this->command == "PRIVMSG")
+		this->type = EntryType::Text;
+
+	this->arguments = join(parts.begin() + 3, parts.end(), " ");
+	if(this->arguments[0] == ':')
+		this->arguments = this->arguments.substr(1);
+}
+
 string Entry::format() const {
 	return asString(this->timestamp) + "|" + this->contents;
 }
+
--- src/journal.hpp
+++ src/journal.hpp
@@ -3,30 +3,31 @@
 
 #include <string>
 #include <vector>
+#include "global.hpp"
 
 namespace journal {
 	enum class EntryType { Text, Join, Quit, Part, Invalid };
 
-	// TODO: log of logitem
 	struct Entry {
-		unsigned long timestamp;
-		std::string contents;
+		long long timestamp{global::now()};
+		std::string contents{};
+		std::string who{}, where{}, command{}, arguments{};
+		EntryType type{EntryType::Invalid};
 
-		Entry() : timestamp(0), contents() {}
+		Entry() = default;
 		Entry(std::string icontents);
-		// parse a line of IRC to cerate a LogItem
-		static Entry parse(std::string line);
+		Entry(long long itimestamp, std::string icontents);
 
-		EntryType type() const;
-		std::string who() const;
-		std::string where() const;
-		std::string message() const;
+		static Entry fromLog(std::string line);
+
+		// parse contents to determine type, w^3, arguments
+		void parse();
 
 		// determines if this is a message addressed to us
 		bool toUs();
 		// formats this object as a printable string
 		std::string toString() const;
-		// formats this object for writing to a file
+		// formats this object for writing to a journal file
 		std::string format() const;
 	};
 
@@ -35,6 +36,7 @@ namespace journal {
 
 	void push(Entry nentry);
 	std::vector<Entry> search(std::string regex);
+	unsigned size();
 }
 
 #endif // JOURNAL_HPP
