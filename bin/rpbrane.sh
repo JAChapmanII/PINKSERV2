8fa6472ac59f0f2e814f452826e4c00a2444a16f
Jeff Chapman II <jac@jac2.net>
2015-08-18 12:23:35 -0400

    rpbrane.sh: run pbrane in separate directory

--- bin/rpbrane.sh
+++ bin/rpbrane.sh
@@ -2,12 +2,19 @@
 
 nick=${1-jac}
 chan=${2-3dpe}
+rdir=${3-run}
 
 if [[ ! -f ./bin/pbrane ]]; then
 	echo "./bin/pbrane does not exist, make sure you make'd!"
 	exit 1
 fi
 
-cat /dev/stdin | sed -ur "s/(.+)/console :$nick!$nick@host PRIVMSG #$chan :\\1/" | \
-	./bin/pbrane
+pbrane="$(realpath "./bin/pbrane")"
+
+mkdir -p "${rdir}"
+cd "${rdir}"
+
+cat /dev/stdin \
+	| sed -ur "s/(.+)/console :$nick!$nick@host PRIVMSG #$chan :\\1/" \
+	| ${pbrane}
 
