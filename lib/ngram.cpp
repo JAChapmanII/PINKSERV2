4eeada28d75a9dd64858e2a4bf47b68d98060e8e
Jeff Chapman II <jac@jac2.net>
2015-08-20 20:26:05 -0400

    Rock the cache-bah. Make ngram reuse all the things.
    This removes the need to recreate all the prepared statements used by the
    ngramStore. Most statements are used many many many times, especially if we
    limit the total order of the model.
    
    This caches the status of tables, if they're created then we never run another
    'create table if not exists'. It caches the constructed string-sql. And
    finally it caches and keeps around the prepared statements made out of said
    strings.

--- lib/ngram.cpp
+++ lib/ngram.cpp
@@ -2,10 +2,15 @@
 using std::vector;
 using std::string;
 using std::to_string;
+using std::map;
 using db::Database;
 using db::Statement;
 
 #include "util.hpp"
+
+#include <iostream>
+using std::cerr;
+using std::endl;
 
 ngram_t::ngram_t(prefix_t iprefix, word_t iatom)
 		: prefix(iprefix), atom(iatom) { }
@@ -18,74 +23,86 @@ chain_t::chain_t(ngram_t ingram, count_t icount)
 
 
 ngramStore::ngramStore(Database &db, std::string baseTableName)
-		: _db(db), _baseTableName(baseTableName) { }
+		: _db(db), _baseTableName(baseTableName), _builder(baseTableName),
+		  _cache(_db, _builder), _tableCache(_cache) { }
 
 chain_t ngramStore::fetch(ngram_t ngram) {
-	createTable(ngram.order());
-	string sql = "SELECT count FROM " + table(ngram.order())
-		+ " WHERE " + where(ngram.order());
-	Statement statement{_db, sql};
+	_tableCache.exists(ngram.order());
+	auto &statement = _cache.ngramFetch(ngram.order());
 	bind(statement, ngram);
 
 	chain_t chain{ngram};
-	int rc = statement.step();
+	auto result = statement.execute();
+	auto rc = result.status();
 
 	if(rc == SQLITE_DONE) { return ngram; }
 	if(rc != SQLITE_ROW) { throw rc; }
 
-	chain.count = statement.getInteger(0);
+	chain.count = result.getInteger(0);
 	return chain;
 }
 void ngramStore::increment(ngram_t ngram) {
-	createTable(ngram.order());
-	string sql = "INSERT INTO " + table(ngram.order())
-		+ " VALUES(" + qmarks(ngram.order()) + ", 1)";
+	_tableCache.exists(ngram.order());
+	int rc = SQLITE_ERROR;
 	if(exists(ngram)) {
-		sql = "UPDATE " + table(ngram.order()) + " SET count = count + 1 "
-			+ " WHERE " + where(ngram.order());
+		auto &statement = _cache.ngramIncrement(ngram.order());
+		bind(statement, ngram);
+		auto result = statement.execute();
+		rc = result.status();
+	} else {
+		auto &statement = _cache.ngramInsert(ngram.order());
+		bind(statement, ngram);
+		auto result = statement.execute();
+		rc = result.status();
 	}
-	Statement statement{_db, sql};
-	bind(statement, ngram);
-
-	int rc = statement.step();
-	if(rc != SQLITE_DONE) { throw rc; }
+	if(rc != SQLITE_DONE) {
+		cerr << "ngramStore::increment: error: " << rc << endl;
+		throw rc;
+	}
 }
 bool ngramStore::exists(ngram_t ngram) {
-	createTable(ngram.order());
-	string sql = "SELECT COUNT(1) FROM " + table(ngram.order())
-		+ " WHERE " + where(ngram.order());
-	Statement statement{_db, sql};
+	_tableCache.exists(ngram.order());
+	auto &statement = _cache.ngramExists(ngram.order());
 	bind(statement, ngram);
 
-	int rc = statement.step();
-	if(rc != SQLITE_ROW) { throw rc; }
+	auto result = statement.execute();
+	int rc = result.status();
+	if(rc != SQLITE_ROW) {
+		cerr << "ngramStore::exists: error: " << rc << endl;
+		throw rc;
+	}
 
-	return (statement.getInteger(0) > 0);
+	return (result.getInteger(0) > 0);
 }
 
-string ngramStore::table(int order) {
+void ngramStore::bind(Statement &statement, ngram_t &ngram) {
+	for(int i = 0; i < ngram.order(); ++i)
+		statement.bind(i + 1, ngram.prefix[i]);
+	statement.bind(ngram.order() + 1, ngram.atom);
+}
+
+
+
+ngramStoreStatementBuilder::ngramStoreStatementBuilder(string baseTableName)
+		: _baseTableName(baseTableName) { }
+string ngramStoreStatementBuilder::table(int order) const {
 	return _baseTableName + "_" + to_string(order);
 }
-string ngramStore::column(int p) {
+string ngramStoreStatementBuilder::column(int p) const {
 	return "s_" + to_string(p);
 }
-string ngramStore::where(int order) {
+string ngramStoreStatementBuilder::where(int order) const {
 	vector<string> clauses;
 	for(int i = 0; i < order; ++i)
 		clauses.push_back(" (" + column(i) + " = ? )");
 	clauses.push_back(" (atom = ?)");
 	return util::join(clauses, " AND ");
 }
-void ngramStore::bind(Statement &statement, ngram_t &ngram) {
-	for(int i = 0; i < ngram.order(); ++i)
-		statement.bind(i + 1, ngram.prefix[i]);
-	statement.bind(ngram.order() + 1, ngram.atom);
-}
-string ngramStore::qmarks(int order) {
+string ngramStoreStatementBuilder::qmarks(int order) const {
 	vector<string> qms{order + 1, "?"}; // include atom
 	return util::join(qms, ", ");
 }
-void ngramStore::createTable(int order) {
+string ngramStoreStatementBuilder::createTable(int order) const {
 	string sql = "CREATE TABLE IF NOT EXISTS " + table(order);
 	vector<string> columns;
 	for(int i = 0; i < order; ++i)
@@ -93,9 +110,73 @@ void ngramStore::createTable(int order) {
 	columns.push_back("atom int");
 	columns.push_back("count int");
 	sql += "( " + util::join(columns, ", ") + " )";
+	return sql;
+}
+string ngramStoreStatementBuilder::ngramExists(int order) const {
+	return "SELECT COUNT(1) FROM " + table(order) + " WHERE " + where(order);
+}
+string ngramStoreStatementBuilder::ngramFetch(int order) const {
+	return "SELECT count FROM " + table(order) + " WHERE " + where(order);
+}
+string ngramStoreStatementBuilder::ngramInsert(int order) const {
+	return "INSERT INTO " + table(order) + " VALUES(" + qmarks(order) + ", 1)";
+}
+string ngramStoreStatementBuilder::ngramIncrement(int order) const {
+	return "UPDATE " + table(order) + " SET count = count + 1 "
+		+ " WHERE " + where(order);
+}
 
-	Statement statement{_db, sql};
-	int rc = statement.step();
+
+StatementCache::StatementCache(Database &db) : _db{db}, _cache{} { }
+StatementCache::~StatementCache() { for(auto &e : _cache) delete e.second; }
+
+Statement &StatementCache::operator[](string sql) {
+	if(_cache.find(sql) != _cache.end())
+		return *_cache[sql];
+	return *(_cache[sql] = new Statement{_db, sql});
+}
+
+ngramStatementCache::ngramStatementCache(
+		Database &db, ngramStoreStatementBuilder builder)
+			: _builder(builder),  _cache(db) { }
+
+Statement &ngramStatementCache::createTable(int order) {
+	if(_tableCache.find(order) == _tableCache.end())
+		_tableCache[order] = _builder.createTable(order);
+	return _cache[_tableCache[order]];
+}
+Statement &ngramStatementCache::ngramExists(int order) {
+	if(_existsCache.find(order) == _existsCache.end())
+		_existsCache[order] = _builder.ngramExists(order);
+	return _cache[_existsCache[order]];
+}
+Statement &ngramStatementCache::ngramFetch(int order) {
+	if(_fetchCache.find(order) == _fetchCache.end())
+		_fetchCache[order] = _builder.ngramFetch(order);
+	return _cache[_fetchCache[order]];
+}
+Statement &ngramStatementCache::ngramInsert(int order) {
+	if(_insertCache.find(order) == _insertCache.end())
+		_insertCache[order] = _builder.ngramInsert(order);
+	return _cache[_insertCache[order]];
+}
+Statement &ngramStatementCache::ngramIncrement(int order) {
+	if(_incrementCache.find(order) == _incrementCache.end())
+		_incrementCache[order] = _builder.ngramIncrement(order);
+	return _cache[_incrementCache[order]];
+}
+
+ngramTableCache::ngramTableCache(ngramStatementCache &cache) : _cache(cache) { }
+
+bool ngramTableCache::exists(int order) {
+	if(_exists.find(order) != _exists.end())
+		return true;
+
+	auto &statement = _cache.createTable(order);
+	auto result = statement.execute();
+	int rc = result.status();
 	if(rc != SQLITE_DONE) { throw rc; }
+
+	return _exists[order] = true;
 }
 
--- lib/ngram.hpp
+++ lib/ngram.hpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <map>
 #include "db.hpp"
 
 using word_t = unsigned;
@@ -25,6 +26,64 @@ struct chain_t {
 	count_t count;
 };
 
+struct StatementCache {
+	StatementCache(db::Database &db);
+	~StatementCache();
+
+	db::Statement &operator[](std::string sql);
+
+	private:
+		db::Database &_db;
+		std::map<std::string, db::Statement *> _cache{};
+};
+
+struct ngramStoreStatementBuilder {
+	ngramStoreStatementBuilder(std::string baseTableName);
+
+	std::string table(int order) const;
+	std::string column(int p) const;
+	std::string where(int order) const;
+	std::string qmarks(int order) const;
+
+	std::string createTable(int order) const;
+	std::string ngramExists(int order) const;
+	std::string ngramFetch(int order) const;
+	std::string ngramInsert(int order) const;
+	std::string ngramIncrement(int order) const;
+
+	private:
+		std::string _baseTableName{};
+};
+
+struct ngramStatementCache {
+	ngramStatementCache(db::Database &db, ngramStoreStatementBuilder builder);
+
+	db::Statement &createTable(int order);
+	db::Statement &ngramExists(int order);
+	db::Statement &ngramFetch(int order);
+	db::Statement &ngramInsert(int order);
+	db::Statement &ngramIncrement(int order);
+
+	private:
+		ngramStoreStatementBuilder _builder;
+		std::map<int, std::string> _tableCache{};
+		std::map<int, std::string> _existsCache{};
+		std::map<int, std::string> _fetchCache{};
+		std::map<int, std::string> _insertCache{};
+		std::map<int, std::string> _incrementCache{};
+		StatementCache _cache;
+};
+
+struct ngramTableCache {
+	ngramTableCache(ngramStatementCache &cache);
+
+	bool exists(int order);
+
+	private:
+		ngramStatementCache &_cache;
+		std::map<int, bool> _exists{};
+};
+
 struct ngramStore {
 	ngramStore(db::Database &db, std::string baseTableName);
 
@@ -33,17 +92,14 @@ struct ngramStore {
 	bool exists(ngram_t ngram);
 
 	private:
-		std::string table(int order);
-		std::string column(int p);
-		std::string where(int order);
 		void bind(db::Statement &statement, ngram_t &ngram);
-		std::string qmarks(int order);
-
-		void createTable(int order);
 
 	private:
 		db::Database &_db;
 		std::string _baseTableName;
+		ngramStoreStatementBuilder _builder;
+		ngramStatementCache _cache;
+		ngramTableCache _tableCache;
 };
 
 #endif // NGRAM_HPP
